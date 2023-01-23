d68b9697ed02d186d4fedfb233145468e5fe0572
kaleytsangbcit <ktsang38@my.bcit.ca>
2023-01-23 14:25:36 -0800

    create weather card

---
+++ components/logo/weathercard.js
@@ -0,0 +1,10 @@
+import styles from '@/styles/Home.module.css'
+
+export default function Logo() {
+    return (
+        <img 
+            className={styles.weather}
+            src = "./placeholder.jpeg" 
+        />
+    )
+}
