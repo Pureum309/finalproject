d2ec13b28b8c9f57ff8f313f405b5608fa6fe9aa
Heidingg <tnguyen480@my.bcit.ca>
2023-02-13 23:55:06 -0800

    create hamburgerMenu branch, add css and make menu works

--- components/logo/index.js
+++ components/logo/index.js
@@ -1,25 +1,44 @@
-import React from 'react'
-import { Router, useRouter } from 'next/router'
 
+import { Router, useRouter } from 'next/router'
+import React, { useState } from 'react';
 import styles from '@/styles/Home.module.css'
 
 export default function Logo({
-    onClickMenu,
+   
 }) {
     const r = useRouter();
+    const [isVisible, setIsVisible] = useState(false);
 
+    const handleToggle = () => {
+      setIsVisible(!isVisible);
+    }
     return (
         <>
-        <img 
+   
+     <nav>
+      <div className={styles.navbar}>
+        <div className={styles.navcontainer}>
+            <input className={styles.checkbox} type="checkbox" name="" id="" />
+            <div className={styles.hamburgerlines}>
+              <span className={styles.line1}></span>
+              <span className={styles.line2}></span>
+              <span className={styles.line3}></span>
+            </div>  
+          <div class="logo">
+          <img 
             className={styles.moodyLogo}
             src = "./placeholder.jpeg" 
             onClick={()=>{r.push("/")}}
         />
-        <div className={styles.container } onClick={onClickMenu}>
-            <div className={styles.bar1}></div>
-            <div className={styles.bar2}></div>
-            <div className={styles.bar3}></div>
+          </div>
+          <div className={styles.menuitems}>
+            <li><a onClick={()=>{r.push("/")}}>Home</a></li>
+            <li><a onClick={()=>{r.push("/products")}}>Product</a></li>
+            <li><a onClick={()=>{r.push("/recommend")}}>Recommend</a></li>
+          </div>
         </div>
+      </div>
+    </nav>
         </>
     )
 }
