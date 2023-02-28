6a90ebdef98772d0bee1d90e9dc5c8ddd9eef4d2
Pureum309 <psun27@my.bcit.ca>
2023-02-27 19:34:04 -0800

    add navbar on the product item page

--- pages/productItem.js
+++ pages/productItem.js
@@ -2,17 +2,23 @@ import Product from '@/components/productItem'
 import SizeChart from '@/components/sizechart'
 import styles from '@/styles/Home.module.css'
 
+import Logo from '@/components/logo'
+
 export default function Image({
     
 
 }){ 
 
-return(
-<main className={styles.productMain}>
-    <Product/>
-    <SizeChart/>
-</main>
-
-)
+return (
+        <>
+            <Logo 
+            onClickMenu={()=>{console.log("menuClickedddd")}}
+            /> 
+            <main className={styles.productMain}>
+                <Product/>
+                <SizeChart/>
+            </main>
+        </>
+    )
 }
 
