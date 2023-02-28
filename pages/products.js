b959cce1ee3a676d70583eb531378b8ae620c8c1
Pureum309 <psun27@my.bcit.ca>
2023-02-28 11:11:59 -0800

    changed the css

--- pages/products.js
+++ pages/products.js
@@ -6,14 +6,16 @@ export default function Product(){
     return(
         <>
             <Logo />
-            <ListItem limit={20} 
-                productMainItem={styles.productMainItem }
-                productItems={styles.productItems}
-                productItem1={styles.productItem1}
-                productItemImage={styles.productItemImage}
-                productItemText={styles.productItemText}
-                keyword = "Women Top and Bottom"
-            />
+            <div  productMainCont={styles.productMainCont }>
+                <ListItem limit={20} 
+                    productMainItem={styles.productMainItem }
+                    productItems={styles.productItems}
+                    productItem1={styles.productItem1}
+                    productItemImage={styles.productItemImage}
+                    productItemText={styles.productItemText}
+                    keyword = "Women Top and Bottom"
+                />
+            </div>
         </>
     )
 }
